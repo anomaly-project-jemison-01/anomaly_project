38c306d97584cc3fc60054c18c5930ee69daea61
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-07-08 15:06:46 -0500

    prepare.py

---
+++ David/prepare.py
@@ -0,0 +1,104 @@
+# # #### Anomaly Detection Project
+# 
+## Finding Anomalies in Code Up Cohorts Curriculum Logs
+# ***
+# ## Prepare
+# 
+# ### Clean up and prepare data obtained to use for exploration and modeling
+# 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Container
+import os
+import seaborn as sns
+import wrangle as w
+
+
+def prepare_data(df):
+
+    df['timestamp'] = df['date'] + ' ' + df['time']
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    # Rename date and time columns to denote that they are log date and times
+    df.rename(columns={"date": "log_date", "time": "log_time"})
+
+    # Rename name column to cohort and id column to cohort_id
+    df.rename(columns={"name": "cohort", "id": "cohort_id"})
+
+    return df
+
+
+#Plots a normalized value count as a percent using catplot
+def category_percentages_by_another_category_col(df, category_a, category_b):
+    """
+    Produces a .catplot with a normalized value count
+    """
+    (df.groupby(category_b)[category_a].value_counts(normalize=True)
+    .rename('percent')
+    .reset_index()
+    .pipe((sns.catplot, 'data'), x=category_a, y='percent', col=category_b, kind='bar', ))
+
+
+def nulls_by_col(df):
+    num_missing = df.isnull().sum()
+    rows = df.shape[0]
+    prcnt_miss = num_missing / rows * 100
+    cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'percent_rows_missing': prcnt_miss})
+    return cols_missing
+
+def nulls_by_row(df):
+    num_missing = df.isnull().sum(axis=1)
+    prcnt_miss = num_missing / df.shape[1] * 100
+    rows_missing = pd.DataFrame({'num_cols_missing': num_missing, 'percent_cols_missing': prcnt_miss})\
+    .reset_index()\
+    .groupby(['num_cols_missing', 'percent_cols_missing']).count()\
+    .rename(index=str, columns={'customer_id': 'num_rows'}).reset_index()
+    return rows_missing
+
+def summarize(df):
+    '''
+    summarize will take in a single argument (a pandas dataframe) 
+    and output to console various statistics on said dataframe, including:
+    # .head()
+    # .info()
+    # .describe()
+    # value_counts()
+    # observation of nulls in the dataframe
+    '''
+    print('=====================================================\n\n')
+    print('Dataframe head: ')
+    print(df.head(3).to_markdown())
+    print('=====================================================\n\n')
+    print('Dataframe info: ')
+    print(df.info())
+    print('=====================================================\n\n')
+    print('Dataframe Description: ')
+    print(df.describe().to_markdown())
+    num_cols = [col for col in df.columns if df[col].dtype != 'O']
+    cat_cols = [col for col in df.columns if col not in num_cols]
+    print('=====================================================')
+    print('DataFrame value counts: ')
+    for col in df.columns:
+        if col in cat_cols:
+            print(df[col].value_counts())
+        else:
+            print(df[col].value_counts(bins=10, sort=False))
+    print('=====================================================')
+    print('nulls in dataframe by column: ')
+    print(nulls_by_col(df))
+    print('=====================================================')
+    print('nulls in dataframe by row: ')
+    print(nulls_by_row(df))
+    print('=====================================================')
+
+def remove_columns(df, cols_to_remove):
+    df = df.drop(columns=cols_to_remove)
+    return df
+
+def handle_missing_values(df, prop_required_columns=0.5, prop_required_row=0.75):
+    threshold = int(round(prop_required_columns * len(df.index), 0))
+    df = df.dropna(axis=1, thresh=threshold)
+    threshold = int(round(prop_required_row * len(df.columns), 0))
+    df = df.dropna(axis=0, thresh=threshold)
+    return df
